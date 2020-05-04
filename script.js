2671249a3f214da2bafde721828decbad89e8e77
Bryan Pero <bryanpero@gmail.com>
2020-05-03 20:34:06 -0400

    finished time blocks js

--- script.js
+++ script.js
@@ -4,8 +4,56 @@ $("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm a"));
 
 var businessHours = moment().startOf("day").add(9, "hours");
 
-//9am
+//9am time block
 
 var time1 = businessHours.add(0, "h");
 time1 = time1.format("hh:mm A");
 $(".block1").text(time1);
+
+//10am time block
+
+var time1 = businessHours.add(1, "h");
+time1 = time1.format("hh:mm A");
+$(".block2").text(time1);
+
+//11am time block
+
+var time1 = businessHours.add(1, "h");
+time1 = time1.format("hh:mm A");
+$(".block3").text(time1);
+
+//12pm time block
+
+var time1 = businessHours.add(1, "h");
+time1 = time1.format("hh:mm A");
+$(".block4").text(time1);
+
+//1pm time block
+
+var time1 = businessHours.add(1, "h");
+time1 = time1.format("hh:mm A");
+$(".block5").text(time1);
+
+//2pm time block
+
+var time1 = businessHours.add(1, "h");
+time1 = time1.format("hh:mm A");
+$(".block6").text(time1);
+
+//3pm time block
+
+var time1 = businessHours.add(1, "h");
+time1 = time1.format("hh:mm A");
+$(".block7").text(time1);
+
+//4pm time block
+
+var time1 = businessHours.add(1, "h");
+time1 = time1.format("hh:mm A");
+$(".block8").text(time1);
+
+//5pm time block
+
+var time1 = businessHours.add(1, "h");
+time1 = time1.format("hh:mm A");
+$(".block9").text(time1);
