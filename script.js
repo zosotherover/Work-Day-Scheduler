2be3c44743a7407f8ef67de6deba5380c45ba12b
Bryan Pero <bryanpero@gmail.com>
2020-05-03 20:22:45 -0400

    worked on moment.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 // Call moment for the date and time
 moment(Date);
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm a"));
+
+var businessHours = moment().startOf("day").add(9, "hours");
+
+//9am
+
+var time1 = businessHours.add(0, "h");
+time1 = time1.format("hh:mm A");
+$(".block1").text(time1);
